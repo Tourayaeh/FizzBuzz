0977dafecb2b80ae79ed16436fcfd0c318930b5c
Tourayaeh <touraya_97_has@hotmail.fr>
2020-07-16 15:53:46 +0200

    Test to check if it is a regular number

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -12,5 +12,9 @@ class TestFizzBuzz(unittest.TestCase):
     def test_multiple_of_three_and_five(self):
         self.assertEqual(fizzbuzz.process(15), 'FizzBuzz')
 
+    def test_regular_number(self):
+        self.assertEqual(fizzbuzz.process(2), 2)
+        self.assertEqual(fizzbuzz.process(98), 98)
+
 if __name__ == '__main__':
     unittest.main()
